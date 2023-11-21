0885aee5c42c1699d42d7850a55a962dbb22e2f5
Sylwester Szychowiak <sylwesterszychowiak72@gmail.com>
2023-11-21 17:43:07 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie dzień 4
+
+var name = "Ewa";
+var sex = "Woman";
+var age = 33;
+var age2 = 29;
+var age3 = 17;
+
+if (sex == "Woman" && age2 == 29)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+if (age3 == 17)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
